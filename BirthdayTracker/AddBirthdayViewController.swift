8749930659fff8f566698de394eb1b325fd64f60
Sergey Lavrov <sergeylavrov@macbook-pro-olesa.local>
2018-12-18 19:07:31 +0300

    Добавлены уведомления, которые убираются после удаления строк. Так же все модули приведены в порядок (удалены лишние комментарии, добавлены блоки и комментарии по месту)

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -8,37 +8,70 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 class AddBirthdayViewController: UIViewController{
     
+    // MARK: - Поля класса
+
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var birthdatePicker: UIDatePicker!
     
+    // MARK: - Первая загрузка
+    
+    // После первой загрузки - ограничиваем выбор даты сегодняшним днём
     override func viewDidLoad() {
         super.viewDidLoad()
         birthdatePicker.maximumDate = Date();
     }
 
+    // MARK: - Нажатие кнопок
+
+    // При нажатии кнопки Сохранить - сохраняем день рождения
     @IBAction func saveTapped(_sender: UIBarButtonItem){
+        
+        // Сохранение значений полей в переменные
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let birthdate = birthdatePicker.date
+        
+        // Получение делегата и контекста
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
+        // Создание записи в БД
         let newBirthday = Birthday(context: context)
         newBirthday.firstName = firstName
         newBirthday.lastName = lastName
         newBirthday.birthdate = birthdate as Date?
         newBirthday.birthdayID = UUID().uuidString
-        
         if let uniqueId = newBirthday.birthdayID{
             print("birthdayID:\(uniqueId)")
         }
         
         do{
+            // Сохранение записи в БД
             try context.save()
+            
+            // Сохранение и настройка уведомления
+            let message = "Сегодня \(firstName) \(lastName) празднует День Рождения!"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            
+            // Настройка триггера уведомления
+            var dateComponents = Calendar.current.dateComponents([.month, .day], from: birthdate)
+            dateComponents.hour = 19
+            dateComponents.minute = 5
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            
+            // Добавление уведомления в центр уведомлений
+            if let identifier = newBirthday.birthdayID {
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
         } catch let error {
             print("Не удалось сохранить из-за ошибки\(error).")
         }
@@ -46,6 +79,7 @@ class AddBirthdayViewController: UIViewController{
         dismiss(animated: true, completion: nil)
     }
     
+    // При нажатии кнопки Отменить - закрываем форму
     @IBAction func cancelTapped(_sender: UIBarButtonItem){
         dismiss(animated: true, completion: nil)
     }
--- BirthdayTracker/BirthdaysTableViewController.swift
+++ BirthdayTracker/BirthdaysTableViewController.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 class BirthdaysTableViewController: UITableViewController {
+    
+    // MARK: - Подготовка данных
     
     var birthdays = [Birthday]()
     let dateFormatter = DateFormatter()
 
+    // После первой загрузки - выставляем формат даты
     override func viewDidLoad() {
         super.viewDidLoad()
         dateFormatter.dateStyle = .full
         dateFormatter.timeStyle = .none
     }
 
+    // Подготовка данных для отображения
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Подготовка делегата, контекста и запроса к базе данных
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         let fetchRequest = Birthday.fetchRequest() as NSFetchRequest<Birthday>
@@ -31,33 +38,40 @@ class BirthdaysTableViewController: UITableViewController {
         let sortDescriptorFirstName = NSSortDescriptor(key: "firstName", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptorLastName, sortDescriptorFirstName]
         
+        // Получение данных из базы данных и показ в таблице
         do{
             birthdays = try context.fetch(fetchRequest)
         } catch let error {
             print("Не удалось загрузить данные из-за ошибки: \(error).")
         }
+        
+        // Обновление данных таблицы
         tableView.reloadData()
     }
     
-    // MARK: - Table view data source
+    // MARK: - Показ таблицы
 
+    // Разбивка таблицы на секции (здесь только 1 секция)
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
+    // Разбивка секции на строки (здесь количество строк = кол-ву записей в БД)
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return birthdays.count
     }
 
+    // Отрисовка строк таблицы
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "birthdayCellIdentifier", for: indexPath)
+        
+        // Получение данных текущей строки из БД
         let birthday = birthdays[indexPath.row]
         let firstName = birthday.firstName ?? ""
         let lastName = birthday.lastName ?? ""
-        cell.textLabel?.text = lastName + " " + firstName
         
+        // Заполнение текущей ячейки таблицы таблицы
+        let cell = tableView.dequeueReusableCell(withIdentifier: "birthdayCellIdentifier", for: indexPath)
+        cell.textLabel?.text = lastName + " " + firstName
         if let date = birthday.birthdate as Date? {
             cell.detailTextLabel?.text = dateFormatter.string(from: date)
         } else {
@@ -66,41 +80,41 @@ class BirthdaysTableViewController: UITableViewController {
         return cell
     }
 
-    // Override to support conditional editing of the table view.
+    // MARK: - Удаление строк таблицы
+    
+    // Включение возможности редактирования таблицы (нужно для удаления строк)
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
 
-    // Override to support editing the table view.
+    // Редактирование строк таблицы
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // Защита от ошибок неверного указания индекса
         if birthdays.count > indexPath.row{
             let birthday = birthdays[indexPath.row]
+            
+            // Удаление уведомления
+            if let identifier = birthday.birthdayID{
+                let center = UNUserNotificationCenter.current()
+                center.removePendingNotificationRequests(withIdentifiers: [identifier])
+            }
+            
+            // Удаление из контекста
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
             let context = appDelegate.persistentContainer.viewContext
             context.delete(birthday)
+            
+            // Удаление строки из БД
             birthdays.remove(at: indexPath.row)
             do{
                 try context.save()
             } catch let error {
                 print("Не удалось сохранить из-за ошибки\(error).")
             }
+            
+            // Удаление строки из таблицы
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-    
 }
